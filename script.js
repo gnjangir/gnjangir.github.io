2dae931b324731385a3d445f9aee5645871403b6
Krishna <62286681+gnjangir@users.noreply.github.com>
2023-06-10 12:58:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let vid = document.querySelector('.video-view');
-vid.addEventListener("mouseover",function(e){
-    vid.play();
-});
-vid.addEventListener("mouseout",function(e){
-    vid.pause();
-});
