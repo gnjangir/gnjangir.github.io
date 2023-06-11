71f83f53e3a4864d3cf5e71297ec1eb073b67da5
Krishna <62286681+gnjangir@users.noreply.github.com>
2023-06-12 00:29:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// let vid = document.querySelector('.video-view');
+// vid.addEventListener("mouseover",function(e){
+//     vid.play();
+// });
+// vid.addEventListener("mouseout",function(e){
+//     vid.pause();
+// });
+// on next update...
